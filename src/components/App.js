02e6012c65fec7c7a40e4ec463941cbb9ed21fe6
Magima Felix O <magmafelix2002@yahoo.com>
2022-05-13 12:06:23 +0200

    Frontend MetaMask-Ganach connected

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 import './App.css'
+import Web3 from 'web3'
+
 
 class App extends Component {
 
@@ -10,6 +12,68 @@ class App extends Component {
       account: '0x0'
     }
   }
+
+
+
+// swap with usestate,useEffect
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+
+//_________loading blockchain data
+async loadBlockchainData() {
+  const web3 = window.web3
+
+  const accounts = await web3.eth.getAccounts()
+  //const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+  this.setState({ account: accounts[0] })
+
+  //___________logs account 1
+    console.log(accounts)
+    const networkId = await web3.eth.net.getId()
+    console.log(networkId)
+}
+
+
+//________________load web3 into the app
+async loadWeb3() {
+   try {
+
+        if (typeof window.ethereum !== 'undefined') {
+         window.web3 = new Web3(window.ethereum)
+         await window.ethereum.enable()
+    }
+    // if (window.ethereum) {
+    //   try {
+    //     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    //     console.log(accounts)
+    //     // setAccounts(accounts);
+    //   } catch (error) {
+    //     if (error.code === 4001) {
+    //       // User rejected request
+    //     }
+    
+    //     // setError(error);
+    //     console.log(error)
+    //   }
+     //}
+  
+  
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('You have not yet installed Metamask on your browser, Please install to use this App')
+    }
+   } catch (error) {
+     console.log(error)
+     
+   }
+}
+
 
   render() {
     return (
